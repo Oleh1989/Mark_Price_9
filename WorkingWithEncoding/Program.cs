7d833087952b875b5118bfd8b8ac4b0c6b785ea8
Олег Бабков <babkoff1989@gmail.com>
2019-08-07 16:41:28 +0300

    WorkWithSerialization added

--- WorkingWithEncoding/Program.cs
+++ WorkingWithEncoding/Program.cs
@@ -16,6 +16,54 @@ namespace WorkingWithEncoding
             WriteLine("[5] UTF-32");
             WriteLine("[any other key] Default");
 
+            // Выбор кодировки
+            Write("Press a number to chose an encoding: ");
+            ConsoleKey number = ReadKey(false).Key;
+            WriteLine();
+            WriteLine();
+
+            Encoding encoder;
+            switch (number)
+            {
+                case ConsoleKey.D1:
+                    encoder = Encoding.ASCII;
+                    break;
+                case ConsoleKey.D2:
+                    encoder = Encoding.UTF7;
+                    break;
+                case ConsoleKey.D3:
+                    encoder = Encoding.UTF8;
+                    break;
+                case ConsoleKey.D4:
+                    encoder = Encoding.Unicode;
+                    break;
+                case ConsoleKey.D5:
+                    encoder = Encoding.UTF32;
+                    break;
+                default:
+                    encoder = Encoding.Default;
+                    break;
+            }
+
+            // Определение строки для кодирования
+            string message = "A pint of milk is $1.99";
+
+            // Кодирование строки в последовательность байтов
+            byte[] encoded = encoder.GetBytes(message);
+
+            // Проверка количества байтов, необходимого для кодирования
+            WriteLine($"{encoder.GetType().Name} uses {encoded.Length} bytes.");
+            WriteLine("Byte   Hex   Char");
+
+            // Перечисление каждого байта
+            foreach (byte b in encoded)
+            {
+                WriteLine($"{b,4} {b.ToString("X"),4} {(char)b,5}");
+            }
+
+            // Декодирование последовательности байтов обратно в строку и её вывод
+            string decoded = encoder.GetString(encoded);
+            WriteLine(decoded);
 
         }
     }
---
+++ WorkingWithSerialization/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;                       // DateTime
+using System.Collections.Generic;   // List<T>, HashSet<T>
+using System.Xml.Serialization;     // XmlSerializer
+using System.IO;                    // FileStream
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+using Newtonsoft.Json;
+
+namespace WorkingWithSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание графа объектов
+            var people = new List<Person>
+            {
+                new Person(3000M) {FirstName = "Alice",     LastName = "Smith",     DateOfBirth = new DateTime(1974, 3, 14)},
+                new Person(4000M) {FirstName = "Bob",       LastName = "Jones",     DateOfBirth = new DateTime(1969, 11, 23)},
+                new Person(20000M) {FirstName = "Charlie",  LastName = "Rose",      DateOfBirth = new DateTime(1964, 5, 4),
+                    Children = new HashSet<Person>
+                    {
+                        new Person(0M) {FirstName = "Sally", LastName = "Rose",     DateOfBirth = new DateTime(1990, 7, 12)}
+                    }
+                }
+            };
+
+            // Создание файла для записи
+            string path = Combine(CurrentDirectory, "people.xml");
+
+            FileStream stream = File.Create(path);
+
+            // Создание объекта, который будет отформатирован в виде
+            // списка людей в формате XML
+            var xs = new XmlSerializer(typeof(List<Person>));
+
+            // Сериализация графа объектов в поток
+            xs.Serialize(stream, people);
+
+            // Необходимо закрыть поток, чтобы разблокировать файл
+            stream.Close();
+
+            WriteLine($"Written {new FileInfo(path).Length} bytes of XML to {path}");
+            WriteLine();
+
+            // Отображение сериализованного графа объектов.
+            WriteLine(File.ReadAllText(path));
+
+
+            FileStream xmlLoad = File.Open(path, FileMode.Open);
+            // Десериализация и привидение графа объектов к списку людей.
+            var loadedPeople = (List<Person>)xs.Deserialize(xmlLoad);
+
+            foreach (var item in loadedPeople)
+            {
+                WriteLine($"{item.LastName} has {item.Children.Count} children");
+            }
+            xmlLoad.Close();
+
+
+            WriteLine(new string('#', 100));
+
+            // Создание файла для записи
+            string jsonPath = Combine(CurrentDirectory, "people.json");
+
+            StreamWriter jsonStream = File.CreateText(jsonPath);
+
+            // Создание объекта для хранения в JSON
+            var jss = new JsonSerializer();
+
+            // Сериализация графа объектов в строку
+            jss.Serialize(jsonStream, people);
+            jsonStream.Close(); // Разблокировать файл
+
+            WriteLine();
+            WriteLine($"Written {new FileInfo(jsonPath).Length} bytes of JSON to {jsonPath}");
+
+            // Отображение сериализованного графа объектов
+            WriteLine(File.ReadAllText(jsonPath));
+        }
+    }
+}
