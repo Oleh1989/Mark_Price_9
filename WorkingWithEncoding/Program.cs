fe40754458a22bb0b5b612bc0f3afffa321af556
Олег Бабков <babkoff1989@gmail.com>
2019-08-07 11:58:26 +0300

    WorkWithEncoding added

---
+++ WorkingWithEncoding/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+
+namespace WorkingWithEncoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Encodings");
+            WriteLine("[1] ASCII");
+            WriteLine("[2] UTF-7");
+            WriteLine("[3] UTF-8");
+            WriteLine("[4] UTF-16 (Unicode)");
+            WriteLine("[5] UTF-32");
+            WriteLine("[any other key] Default");
+
+
+        }
+    }
+}
